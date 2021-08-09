552b79fdf1dc21fd9af5f3bccd0e4169b8e2790d
u0000000021 <u0000000021@uontario.ca>
2021-08-09 04:58:22 -0400

    .

---
+++ gitops-ci.sh
@@ -0,0 +1,30 @@
+export VERSION=$(git rev-parse HEAD | cut -c1-7)
+make build
+make test
+
+export NEW_IMAGE="uontario/sample-app:${VERSION}"
+docker build -t ${NEW_IMAGE} .
+docker push ${NEW_IMAGE}
+
+git clone http://github.com/uontario/sample-app-deployment.git
+cd sample-app-deployment
+
+kubectl patch \
+  --local \
+  -o yaml \
+  -f deployment.yaml \
+  -p "spec:
+        template:
+          spec:
+            containers:
+            - name: sample-app
+              image: ${NEW_IMAGE}" \
+  > /tmp/newdeployment.yaml
+mv /tmp/newdeployment.yaml deployment.yaml
+
+git commit deployment.yaml -m "Update sample-app image to ${NEW_IMAGE}"
+git push
+
+cd ..
+rm -rf sample-app-deployment
+
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-const name = "Golang"
+const name = "UoF"
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
